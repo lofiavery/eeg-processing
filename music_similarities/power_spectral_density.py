2d05d03a1fb7594a802d1965eb3e28d55d4ebad6
lofiavery <lofiavery@proton.me>
2018-08-25 17:19:32 -0400

    Create power_spectral_density.py

---
+++ music_similarities/power_spectral_density.py
@@ -0,0 +1,25 @@
+"""
+Created on Sat Aug 25 14:50:05 2018
+
+@author: Malte Gueth
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import mne
+from mne.time_frequency import psd_multitaper
+
+# Load epoched data segments and compute power spectral density for all genres
+# as alternative input for the rsa script
+file = './epochs/sub-03-reordered-epo.fif'
+epochs = mne.read_epochs(file)
+
+picks = mne.pick_types(epochs.info, eeg=True)
+tmin, tmax = 0, 1 # Pick time window within each epoch
+fmin, fmax = 1, 60 # Pick frequency range
+
+# Besides frequencies, the function returns an array of 320 epochs x 64 electrodes x 59 frequencies
+psds, freqs = psd_multitaper(epochs, tmin=tmin, tmax=tmax,
+                             fmin=fmin, fmax=fmax, picks=picks)
+                             
